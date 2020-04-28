2f6c4177cf8bcb10226018291a2c256d6607fdc3
unknown <vinayasenareddy.k@gmail.com>
2020-04-28 15:19:54 +0530

    initial first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import JavascriptTimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+import fr from 'javascript-time-ago/locale/fr';
+// ionic css
+import "@ionic/react/css/core.css";
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+// end ionic
+// imports for ionic
+import {
+  IonApp,
+  
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import * as serviceWorker from "./serviceWorker";
+import configureStore from './store';
+import Root from './containers/Root';
 
+JavascriptTimeAgo.locale(en); 
+JavascriptTimeAgo.locale(fr);
+
+const store = configureStore();
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.Fragment>
+    <IonApp>
+      <IonReactRouter>
+        <Root store={store}/>        
+      </IonReactRouter>
+    </IonApp>
+  </React.Fragment>, 
+  document.getElementById("root")  
 );
 
 // If you want your app to work offline and load faster, you can change
